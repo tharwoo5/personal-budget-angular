c87412532e41137c762757c49225e65c3efc0f24
Trevor Harwood <tharwoo5@uncc.edu>
2020-10-03 14:37:39 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,36 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
-
+app.use("/", express.static("public"));
+/*
 const budget = {
-  myBudget:  [
-
-{
-    title: 'Eat out',
-    budget: 25
-},
-{
-    title: 'Rent',
-    budget: 375
-},
-{
-    title: 'Grocery',
-    budget: 110
-},
-
-    ]
+  myBudget: [
+    {
+      title: "Eat out",
+      budget: 25,
+    },
+    {
+      title: "Rent",
+      budget: 375,
+    },
+    {
+      title: "Grocery",
+      budget: 110,
+    },
+  ],
 };
+*/
+const budget = require("./server_budget");
 
-app.get('/hello', (req,res) =>{
-    res.send('Hello World!');
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
 });
-
-app.get('/budget', (req,res) =>{
-    res.json(budget);
+app.get("/budget", (req, res) => {
+  res.json(budget);
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
